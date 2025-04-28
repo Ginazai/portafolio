ae36ec55726a2c4d3141223011a0bc664e2ebe22
Ginazai <rafaeldc1300@gmail.com>
2025-04-28 13:53:57 -0500

    Transitions added for text on page load. increased list of projects. Added links to projects and modified <a> tag aesthetics. Added library for pagination for later implementing.

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(() => {
+  const light = document.getElementById('light');
+  const container = document.getElementById('body');
+
+  container.addEventListener('mousemove', (e) => {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    light.style.left = `${x}px`;
+    light.style.top = `${y}px`;
+  });
+  $(".fa-github").click(()=>{window.open("https://github.com/Ginazai", "_blank");});
+  $(".fa-linkedin-square").click(()=>{window.open("https://www.linkedin.com/in/rafael-caballero-ba226323a/", "_blank");});
+  $(".fa-envelope").click(()=>{window.open("mailto:rafaeldc1300@gmail.com", "_blank");});
+  //theme controller
+  $("#theme").on('click', () => {
+    const theme_elem = $("#theme");
+    const current_theme = theme_elem.hasClass("fa-sun-o");
+
+    if(current_theme){
+      $("html").attr("data-bs-theme", "dark");
+      theme_elem.removeClass("fa-sun-o");
+      theme_elem.addClass("fa-moon-o");
+      $("#light").css("background", "white");
+    } else {
+      $("html").attr("data-bs-theme", "light");
+      theme_elem.removeClass("fa-moon-o");
+      theme_elem.addClass("fa-sun-o");
+      $("#light").css("background", "#D2FF1F");
+    }
+  });
+  $("body").mouseover(() => {
+    const theme_elem = $("#theme");
+    const current_theme = theme_elem.hasClass("fa-sun-o");
+
+    if(current_theme){
+      $("body").css("background-position", "top right");
+    } else {
+      $("body").css("background-position", "bottom right");
+    }
+  });
+  $("body").mouseleave(() => {
+    const theme_elem = $("#theme");
+    const current_theme = theme_elem.hasClass("fa-sun-o");
+
+    if(current_theme){
+      $("body").css("background-position", "bottom left");
+    } else {
+      $("body").css("background-position", "top left");
+    }
+  });
+  // $('#demo').pagination({
+  //   dataSource: [1, 2, 3, 4, 5, 6, 7, ... , 195],
+  //   callback: function(data, pagination) {
+  //       // template method of yourself
+  //       var html = template(data);
+  //       dataContainer.html(html);
+  //   }
+  // });
+});
