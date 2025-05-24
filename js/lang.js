c1695976230fcfec8b63e450d3732725b0e11758
Ginazai <rafaeldc1300@gmail.com>
2025-05-24 13:21:21 -0500

    fixed minor lang issue.

--- js/lang.js
+++ js/lang.js
@@ -5,6 +5,7 @@ lang =
 		"greeting" : "Bienvenido a mi portafolio!",
 		"name" : "Mi nombre es Rafael Caballero, ingeniero de sistemas informaticos en formacion",
 		"desc" : "Aquí comparto una parte de mi proceso: soluciones que buscan ser funcionales, claras y sostenibles, con cada línea de código pensada para integrarse con precisión a un sistema. No se trata solo de saber hacer, tambien de cómo abordarlo: con profundidad, intención y una constante búsqueda de mejorar. Cada proyecto refleja mi forma de aprender, razonar y construir.",
+		"projects title" : "Proyectos",
 		"links" : data_links_es
 	},
 	{
@@ -12,6 +13,7 @@ lang =
 		"greeting" : "Welcome to my portfolio!",
 		"name" : "My name is Rafael Caballero, computer systems engineer in training",
 		"desc" : "Here I share a part of my process: solutions that strive to be functional, clear, and sustainable, with every line of code designed to integrate precisely into a system. It's not just about know-how, but also how to approach it: with depth, intention, and a constant pursuit of improvement. Each project reflects my way of learning, reasoning, and building.",
+		"projects title" : "Projects",
 		"links" : data_links_en
 	}
 ];
